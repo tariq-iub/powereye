923054f6356c2757691be4eaa15b6e82f7a4c137
Muhammad Tariq <m.tariq@iub.edu.pk>
2024-05-29 22:27:21 +0500

    Basic layout setting

---
+++ app/Models/Menu.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title'];
+
+    public function subMenus()
+    {
+        return $this->hasMany(Menu::class, 'parent_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Menu::class, 'parent_id');
+    }
+
+    public function parentsOnly()
+    {
+        return self::where('parent_id', null)->with('subMenus')->get();
+    }
+}

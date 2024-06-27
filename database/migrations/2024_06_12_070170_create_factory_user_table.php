f9e7c5cb482bedf6c9ff729635229132c8d0991a
Muhammad Tariq <m.tariq@iub.edu.pk>
2024-06-27 19:11:13 +0500

    Factory user issue

--- database/migrations/2024_06_12_070170_create_factory_user_table.php
+++ database/migrations/2024_06_12_070170_create_factory_user_table.php
@@ -17,9 +17,6 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->enum('access_level', ['owner', 'employee'])->default('employee');
             $table->timestamps();
-
-            $table->foreign('factory_id')->references('id')->on('factories')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -28,11 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::disableForeignKeyConstraints();
-//        Schema::table('factory_user', function (Blueprint $table) {
-//            $table->dropForeign(['factory_id']);
-//            $table->dropForeign(['user_id']);
-//        });
         Schema::dropIfExists('factory_user');
     }
 };

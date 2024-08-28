de4fdfb6708c365b9de2a0d1e526aa8d737e7b72
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2024-08-28 19:06:58 +0500

    Updated Site Chart

--- resources/views/dashboard/client.blade.php
+++ resources/views/dashboard/client.blade.php
@@ -54,7 +54,7 @@
                                                         </span>
                                                     </h6>
                                                 </div>
-                                                <div id="siteGaugeChart-{{ $site->id }}" class="chart" style="width:200px; height: 100px"></div>
+                                                <div id="siteGaugeChart-{{ $site->id }}" class="chart" style="width:120px; height: 100px"></div>
                                             </div>
                                         </div>
                                     </div>
@@ -153,23 +153,95 @@
                 // Gauge Charts for Sites
                 factory.sites.forEach(site => {
                     const gaugeChart = echarts.init(document.getElementById(`siteGaugeChart-${site.id}`));
-                    gaugeChart.setOption({
-                        title: { text: `${site.title} Energy Usage`, left: 'center' },
-                        series: [{
-                            type: 'gauge',
-                            radius: '80%',
-                            center: ['50%', '60%'],
-                            min: 0,
-                            max: 1,
-                            data: [{ value: site.totalEnergy }],
-                            axisLine: {
-                                lineStyle: { color: [[0.2, 'green'], [0.8, 'yellow'], [1, 'red']], width: 10 }
-                            },
-                            pointer: { width: 5 },
-                            detail: { formatter: '{value} kWh', fontSize: 16 }
-                        }]
-                    });
+
+                    const gOption = {
+                        series: [
+                            {
+                                type: 'gauge',
+                                startAngle: 180,
+                                endAngle: 0,
+                                center: ['50%', '70%'],
+                                radius: '100%',
+                                min: 0,
+                                max: 240,
+                                splitNumber: 12,
+                                progress: {
+                                    show: false,
+                                    roundCap: true,
+                                    width: 6
+                                },
+                                pointer: {
+                                    icon: 'path://M2090.36389,615.30999 L2090.36389,615.30999 C2091.48372,615.30999 2092.40383,616.194028 2092.44859,617.312956 L2096.90698,728.755929 C2097.05155,732.369577 2094.2393,735.416212 2090.62566,735.56078 C2090.53845,735.564269 2090.45117,735.566014 2090.36389,735.566014 L2090.36389,735.566014 C2086.74736,735.566014 2083.81557,732.63423 2083.81557,729.017692 C2083.81557,728.930412 2083.81732,728.84314 2083.82081,728.755929 L2088.2792,617.312956 C2088.32396,616.194028 2089.24407,615.30999 2090.36389,615.30999 Z',
+                                    length: '50%',
+                                    width: 8,
+                                    offsetCenter: [0, '-40%'],
+                                    itemStyle: {
+                                        color:'auto'
+                                    }
+                                },
+                                axisLine: {
+                                    roundCap: true,
+                                    lineStyle: {
+                                        width: 8,
+                                        color: [
+                                            [0.25, '#0f0'],  // Green
+                                            [0.75, '#ff0'],  // Yellow
+                                            [1, '#f00']      // Red
+                                        ]
+                                    }
+                                },
+                                axisTick: {
+                                    show: false
+                                },
+                                splitLine: {
+                                    show: false,
+                                },
+                                axisLabel: {
+                                    show: false
+                                },
+                                title: {
+                                    show: false
+                                },
+                                detail: {
+                                    backgroundColor: '#fff',
+                                    width: '60%',
+                                    lineHeight: 15,
+                                    height: 15,
+                                    borderRadius: 4,
+                                    offsetCenter: [0, '30%'],
+                                    valueAnimation: true,
+                                    formatter: function (value) {
+                                        return `${value} kWh`;
+                                    },
+                                    fontSize: 16
+                                },
+                                data: [
+                                    {
+                                        value: site.totalEnergy || 0  // Use the dynamic value here
+                                    }
+                                ]
+                            }
+                        ]
+                    };
+
+
+                    gaugeChart.setOption(gOption);
                     charts[`siteGaugeChart-${site.id}`] = gaugeChart;
+
+                    function updateGaugeChart(value) {
+                        gaugeChart.setOption({
+                            series: [
+                                {
+                                    data: [
+                                        { value: value }
+                                    ]
+                                }
+                            ]
+                        });
+                    }
+
+                    updateGaugeChart(site.totalEnergy);
+
                 });
             });
 

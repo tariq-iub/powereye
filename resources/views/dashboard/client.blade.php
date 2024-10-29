5610a7c5ee761b882b445d220aee9e29de2c42e4
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2024-10-29 09:35:19 +0500

    fix: fix timeframe default to 1d in client dashboard

--- resources/views/dashboard/client.blade.php
+++ resources/views/dashboard/client.blade.php
@@ -305,7 +305,7 @@
                 initChart(`factoryEnergyDough-${factory.id}`, doughnutOption('Energy Distribution', doughnutData));
                 initChart(`factoryEnergyBar-${factory.id}`, barOption([], [{ name: 'Energy (kWh)', data: [] }]));
 
-                const timeframe = '1w';
+                const timeframe = '1d';
 
                 fetchData(`sensor-data/factory/${factory.id}?startDate=${timeframe}`).then(data => {
                     if (!data) return;

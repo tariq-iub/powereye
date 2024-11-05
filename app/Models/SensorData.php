b416bdb0e92291ce7f0d2e24b883d57a1dbbb2c6
Abdullah Abid <abdrps2004@gmail.com>
2024-11-05 23:06:12 +0500

    added the total energy and total power in the fillables in sensor data model

--- app/Models/SensorData.php
+++ app/Models/SensorData.php
@@ -9,7 +9,14 @@ class SensorData extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['data_file_id', 'timestamp', 'V1', 'I1', 'P1', 'Q1', 'E1', 'temperature'];
+    protected $fillable = [
+        'data_file_id', 'timestamp',
+        'V1', 'I1', 'P1', 'Q1', 'E1',
+        'V2', 'I2', 'P2', 'Q2', 'E2',
+        'V3', 'I3', 'P3', 'Q3', 'E3',
+        'total_power', 'total_energy',
+        'temperature'
+    ];
     protected $dates = ['timestamp'];
 
     public function data_file()

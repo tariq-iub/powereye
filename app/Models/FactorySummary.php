f84e831309bd087d713f23dfad44d7016409ddb2
Abdullah Abid <abdrps2004@gmail.com>
2024-11-09 17:36:32 +0500

    Created Factory and Site summary models.

---
+++ app/Models/FactorySummary.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FactorySummary extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'factory_id',
+        'time_frame',
+        'power',
+        'energy',
+        'min_power',
+        'max_power',
+        'min_energy',
+        'max_energy',
+        'start_time',
+        'end_time',
+    ];
+}
---
+++ app/Models/SiteSummary.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SiteSummary extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'site_id',
+        'time_frame',
+        'power',
+        'energy',
+        'min_power',
+        'max_power',
+        'min_energy',
+        'max_energy',
+        'start_time',
+        'end_time',
+    ];
+}
